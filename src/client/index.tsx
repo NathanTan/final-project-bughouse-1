e7e82f228cefb30bfe8fb43a68e2e7636eacf939
Rikki Gibson <rikkigibson@gmail.com>
2017-05-28 16:25:57 -0700

    Add basic socket communication

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,37 @@
 
+interface State {
+	count: number;
+	data: string;
+}
+
+class App {
+	sock: SocketIOClient.Socket;
+	state = {
+		count: 0,
+		data: ''
+	};
+
+	constructor(private rootElem: HTMLElement) {
+		this.sock = io();
+		this.sock.on('eh', this.onEh);
+	}
+
+	onEh = (data: any) => {
+		const newState = {...this.state};
+		newState.count++;
+		newState.data = data.foo;
+		this.state = newState;
+		this.render();
+	};
+
+	render() {
+		ReactDOM.render(
+			<div>
+				<p>I have eh'd {this.state.count} times.</p>
+				<p>Here is my data: {this.state.data}</p>
+			</div>, this.rootElem);
+	}
+}
+
 const elem = document.getElementById('root')!;
-ReactDOM.render(
-	<div>
-		Here's my stuff: {42}
-	</div>, elem);
+const app = new App(elem);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
+import * as http from 'http';
 import * as handlebars from 'express-handlebars';
 import * as path from 'path';
+import * as socket from 'socket.io';
 
 const app = express();
+const server = (http as any).Server(app);
+const sock = socket.listen(server);
 
 app.engine('handlebars', handlebars({
 	defaultLayout: 'main',
@@ -16,4 +20,15 @@ app.get('/', function(req, res) {
 	res.render('home');
 });
 
-app.listen(3000);
+sock.on('connection', function(sock) {
+	console.log("A user connected");
+	repeating(sock);
+});
+
+function repeating(sock: SocketIO.Socket) {
+	console.log("Sending message...");
+	sock.emit('eh', { foo: "My data, received through a socket, from a server!!" });
+	setTimeout(() => repeating(sock), 1000);
+}
+
+server.listen(3000);
