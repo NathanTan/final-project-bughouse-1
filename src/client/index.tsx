c089276f85bd149b4dda5de83ecaa16d868d73ec
Kyler Stole <kylerstole@gmail.com>
2017-06-03 00:01:59 -0700

    Prevent player from moving pieces when it is not their turn and update the board on moves to support castling, en passant, and pawn promotion

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -23,7 +23,9 @@ class App {
 		const board1Config: ChessBoardJS.BoardConfig = {
 			showNotation: false,
 			draggable: true,
-			onDrop: this.onDrop.bind(this)
+			onDrop: this.onDrop.bind(this),
+			onDragStart: this.onDragStart.bind(this),
+			onSnapEnd: this.onSnapEnd.bind(this)
 		}
 		const board2Config: ChessBoardJS.BoardConfig = {
 			showNotation: false,
@@ -50,6 +52,21 @@ class App {
 		this.board2.move(move.from + "-" + move.to);
 	}
 
+	// do not pick up pieces if the game is over
+	// only pick up pieces for the side to move
+	onDragStart = (
+		source: string,
+		piece: string,
+		position: string,
+		orientation: string) => {
+		console.log("onDragStart");
+		if (this.game.game_over() === true ||
+			(this.game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+			(this.game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+			return false;
+		}
+	}
+
 	onDrop = (
 		source: string,
 		target: string,
@@ -61,15 +78,20 @@ class App {
 		const move = this.game.move({
 			from: source,
 			to: target,
-			promotion: 'q'
+			promotion: 'q' // TODO: allow user to pick promotion piece
 		});
 
-		if (!move) {
-			return 'snapback';
-		}
+		// illegal move
+		if (!move) return 'snapback';
 
 		console.log("I'm sending a move", move);
 		this.sock.emit('move', move);
+	}
+
+	// update the board position after the piece snap 
+	// for castling, en passant, pawn promotion
+	onSnapEnd = () => {
+		this.board1.position(this.game.fen());
 	}
 }
 
