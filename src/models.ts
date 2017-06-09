8ac51dc2ef92ba10cb75e7a4db491d459b85f133
Rikki Gibson <rikkigibson@gmail.com>
2017-06-09 11:46:07 -0700

    Remove junk from models.ts

--- src/models.ts
+++ src/models.ts
@@ -1,16 +1,4 @@
 
-interface Piece {
-	color: "black" | "white";
-	type: "pawn" | "knight" | "bishop" | "rook" | "king" | "queen";
-	promoted: boolean;
-}
-
-var myPiece = {
-	color: "black",
-	type: "queen",
-	promoted: true
-};
-
 export interface MoveData {
 	board: string;
 	move: ChessJS.Move;
